10226ba768fe7a1afaf58ae999acff88f2ea5a65
JacquesRaoult <raoult.jacques@gmail.com>
2026-01-18 07:34:11 +0100

    Change icons

--- src/App.vue
+++ src/App.vue
@@ -9,37 +9,37 @@
       <ion-content class="ion-padding">
         <ion-list>
           <ion-item button @click="navigateTo('home')">
-            <ion-icon :icon="home" slot="start"></ion-icon>
+            <ion-icon :src="'/assets/ui/house.svg'" slot="start"></ion-icon>
             <ion-label>Accueil</ion-label>
           </ion-item>
 
           <ion-item button @click="navigateTo('offices')">
-            <ion-icon :icon="business" slot="start"></ion-icon>
+            <ion-icon :src="'/assets/ui/church.svg'" slot="start"></ion-icon>
             <ion-label>Offices</ion-label>
           </ion-item>
 
           <ion-item button @click="navigateTo('calendar')">
-            <ion-icon :icon="calendar" slot="start"></ion-icon>
+            <ion-icon :src="'/assets/ui/calendar.svg'" slot="start"></ion-icon>
             <ion-label>Calendrier</ion-label>
           </ion-item>
 
           <ion-item button @click="navigateTo('map')">
-            <ion-icon :icon="map" slot="start"></ion-icon>
+            <ion-icon :src="'/assets/ui/map.svg'" slot="start"></ion-icon>
             <ion-label>Carte</ion-label>
           </ion-item>
 
           <ion-item button @click="navigateTo('prayers')">
-            <ion-icon :icon="heart" slot="start"></ion-icon>
+            <ion-icon :src="'/assets/ui/praying.svg'" slot="start"></ion-icon>
             <ion-label>Prières</ion-label>
           </ion-item>
 
           <ion-item button @click="navigateTo('synaxar')">
-            <ion-icon :icon="book" slot="start"></ion-icon>
+            <ion-icon :src="'/assets/ui/book.svg'" slot="start"></ion-icon>
             <ion-label>Synaxaire</ion-label>
           </ion-item>
 
           <ion-item button @click="navigateTo('about')">
-            <ion-icon :icon="informationCircle" slot="start"></ion-icon>
+            <ion-icon :src="'/assets/ui/info.svg'" slot="start"></ion-icon>
             <ion-label>À propos</ion-label>
           </ion-item>
         </ion-list>
